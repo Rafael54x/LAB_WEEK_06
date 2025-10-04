974eb5bb93dcb933a334200c1042c71aa1760ed4
rafael <rafael.natanael46@gmail.com>
2025-10-04 14:46:33 +0700

    Commit No.2 - Clickable RecyclerView

--- app/src/main/java/com/example/lab_week_06/CatViewHolder.kt
+++ app/src/main/java/com/example/lab_week_06/CatViewHolder.kt
@@ -8,11 +8,14 @@ import com.example.lab_week_06.model.CatBreed
 import com.example.lab_week_06.model.CatModel
 import com.example.lab_week_06.model.Gender
 
-private val FEMALE_SYMBOL = "\u2640"
-private val MALE_SYMBOL = "\u2642"
+private const val FEMALE_SYMBOL = "♀"
+private const val MALE_SYMBOL = "♂"
 private const val UNKNOWN_SYMBOL = "?"
-class CatViewHolder(containerView: View, private val imageLoader:
-ImageLoader) : RecyclerView.ViewHolder(containerView) {
+class CatViewHolder(
+    private val containerView: View,
+    private val imageLoader: ImageLoader,
+    private val onClickListener: CatAdapter.OnClickListener
+) : RecyclerView.ViewHolder(containerView) {
     private val catBiographyView: TextView by lazy {
         containerView.findViewById(R.id.cat_biography) }
     private val catBreedView: TextView by lazy {
@@ -23,14 +26,17 @@ ImageLoader) : RecyclerView.ViewHolder(containerView) {
         containerView.findViewById(R.id.cat_name) }
     private val catPhotoView: ImageView by lazy {
         containerView.findViewById(R.id.cat_photo) }
+
     fun bindData(cat: CatModel) {
+        containerView.setOnClickListener {
+            onClickListener.onItemClick(cat)
+        }
         imageLoader.loadImage(cat.imageUrl, catPhotoView)
         catNameView.text = cat.name
         catBreedView.text = when (cat.breed) {
             CatBreed.AmericanCurl -> "American Curl"
             CatBreed.BalineseJavanese -> "Balinese-Javanese"
             CatBreed.ExoticShorthair -> "Exotic Shorthair"
-            else -> "Unknown"
         }
         catBiographyView.text = cat.biography
         catGenderView.text = when (cat.gender) {
--- app/src/main/java/com/example/lab_week_06/MainActivity.kt
+++ app/src/main/java/com/example/lab_week_06/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.lab_week_06
 
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -13,17 +14,18 @@ class MainActivity : AppCompatActivity() {
         findViewById(R.id.recycler_view)
     }
     private val catAdapter by lazy {
-        CatAdapter(layoutInflater, GlideImageLoader(this))
+        CatAdapter(layoutInflater, GlideImageLoader(this), object:
+                CatAdapter.OnClickListener {
+            override fun onItemClick(cat: CatModel) = showSelectionDialog(cat)
+        })
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         recyclerView.adapter = catAdapter
-        recyclerView.layoutManager = LinearLayoutManager(
-            this,
-            LinearLayoutManager.VERTICAL, false
-        )
+        recyclerView.layoutManager = LinearLayoutManager(this,
+            LinearLayoutManager.VERTICAL, false)
         catAdapter.setData(
             listOf(
                 CatModel(
@@ -49,5 +51,12 @@ class MainActivity : AppCompatActivity() {
                 )
             )
         )
+    }
+
+    private fun showSelectionDialog(cat: CatModel) {
+        AlertDialog.Builder(this)
+            .setTitle("Cat Selected")
+            .setMessage("You have selected cat ${cat.name}")
+            .setPositiveButton("OK") { _, _ -> }.show()
     }
 }
